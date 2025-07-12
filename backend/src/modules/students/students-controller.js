62523bcdd92148828b67ec1af1177177ea65c1da
Yassien <11986226+YassienW@users.noreply.github.com>
2025-07-12 09:37:10 +0300

    Implement students service calls in students-controller

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -3,27 +3,38 @@ const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updat
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
     //write your code
-
+    const { name, className, section, roll } = req.query;
+    const students = await getAllStudents({ name, className, section, roll });
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
     //write your code
-
+    const message = await addNewStudent(req.body);
+    res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
+    const { id: userId } = req.params;
     //write your code
-
+    const message = await updateStudent({...req.body, userId});
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
     //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(id)
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
     //write your code
-
+    const { id: userId } = req.params;
+    const { id: reviewerId } = req.user;
+    const {status} = req.body
+    const message= await setStudentStatus({ userId, reviewerId, status })
+    res.json(message)
 });
 
 module.exports = {
